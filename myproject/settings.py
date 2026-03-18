cf7a505394e8f1914a34da8c653dfeafb82c2107
Django Developer <user@example.com>
2026-03-18 12:25:35 +0530

    Enable debug mode

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#l&2*w-cn^a%g83j*h&fxqh9cf=1q+q8wn($lfyu-ie+d$bl4v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
